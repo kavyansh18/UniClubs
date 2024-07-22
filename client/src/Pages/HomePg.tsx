87a68c019a0982de7bbc1d72afdc4026ca66d4d8
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-22 15:28:27 +0530

    feat:font

--- client/src/Pages/HomePg.tsx
+++ client/src/Pages/HomePg.tsx
@@ -20,14 +20,14 @@ export default function HomePg() {
           initial={{ opacity: 0, y: -70 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.5, duration: 0.8, ease: "easeInOut" }}
-          className="flex justify-center items-center absolute lg:bottom-[11rem] lg:left-[8rem] bottom-[21rem] left-[4.3rem]"
+          className="flex justify-center items-center absolute lg:bottom-[11rem] lg:left-[8rem] bottom-[21rem] left-[5.3rem]"
         >
           <img className="w-24" src={logouc} alt="Logo" />
         </motion.div>
-        <p className="py-1 bg-gradient-to-r from-stone-500 to-stone-700 bg-clip-text text-transparent text-center text-6xl font-medium tracking-tight  md:text-6xl lg:relative absolute lg:bottom-0 bottom-[12rem] lg:left-0 left-[0rem]">
+        <p className="py-1 bg-gradient-to-r from-stone-500 to-stone-700 bg-clip-text text-transparent text-center text-6xl font-medium tracking-tight  md:text-6xl lg:relative absolute lg:bottom-0 bottom-[12rem] lg:left-0 left-[1.5rem]">
           UniClubs
         </p>
-        <p className="bg-gradient-to-br from-slate-300 to-slate-500 py-1 bg-clip-text text-center text-3xl font-medium tracking-tight text-transparent lg:text-5xl lg:mb-0 mb-20 font-odibee-sans tracking-wide">
+        <p className="bg-gradient-to-br from-slate-300 to-slate-500 py-1 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent lg:text-5xl lg:mb-0 mb-20 font-odibee-sans tracking-wide">
           Discover. Connect. Excel.
         </p>
       </motion.h1>
