8d78e5411c69d4b897f0818670b5b13514020d0c
kavyansh18 <kavyanshkumar18@gmail.com>
2024-08-03 18:00:41 +0530

    feat:add-linktree

--- client/src/Components/UI/Lamp.tsx
+++ client/src/Components/UI/Lamp.tsx
@@ -110,10 +110,10 @@ export const Lamp = ({
       </div>
 
       <motion.div
-            initial={{ opacity: 0, y: 50 }}
+            initial={{ opacity: 0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
-            className="w-full absolute bottom-7 right-0 flex justify-center items-center"
+            className="w-full z-50 absolute bottom-7 right-0 flex justify-center items-center"
           >
             <div className="text-white text-[12px]">
               Developed with ❤️ by <span><a href="https://linktr.ee/kavyansh.in" className="hover:text-cyan-400 transition-colors duration-300 ease-in-out">Kavyansh</a></span>
