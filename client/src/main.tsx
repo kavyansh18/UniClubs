4319d10a24cf34ddb8f4d152f43f64904d26a8d9
kavyansh18 <kavyanshkumar18@gmail.com>
2024-08-03 15:26:10 +0530

    fix:trash-code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import HomePg from "./Pages/HomePg";
 import Clubs from "./Pages/Clubs";
-//import Clubs from "./Pages/Clubss";
 
 
 ReactDOM.render(
