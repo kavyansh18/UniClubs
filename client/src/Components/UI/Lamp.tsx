34d21ce4946d45e446c4e0a61993f62566ae9fda
kavyansh18 <kavyanshkumar18@gmail.com>
2024-08-03 18:09:31 +0530

    fix:home-pos

--- client/src/Components/UI/Lamp.tsx
+++ client/src/Components/UI/Lamp.tsx
@@ -97,7 +97,7 @@ export const Lamp = ({
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
-            className="w-full absolute top-[15rem] right-0 flex justify-center items-center"
+            className="w-full absolute lg:top-[16rem] top-[12rem] right-0 flex justify-center items-center"
           >
             <div className="text-white text-[16px] hover:text-cyan-400 transition-colors duration-300 ease-in-out">
               Let's find perfect club for you in SRM
@@ -113,7 +113,7 @@ export const Lamp = ({
             initial={{ opacity: 0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.8, ease: "easeInOut" }}
-            className="w-full z-50 absolute bottom-12 right-0 flex justify-center items-center"
+            className="w-full z-50 absolute lg:bottom-9 bottom-[8rem] right-0 flex justify-center items-center"
           >
             <div className="text-white text-[13px]">
               Developed with ❤️ by <span><a href="https://linktr.ee/kavyansh.in" className="hover:text-cyan-400 transition-colors duration-300 ease-in-out">Kavyansh</a></span>
